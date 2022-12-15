18fbc07761592f8172673cb90d9d32d6caedd6d9
Franssen <ngoiefranssen04@gmail.com>
2022-12-15 11:18:41 +0200

    Wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from '@mui/icons-material/button';
 
 function App() {
   return (
     <div className="App">
       Hello ! I'm App controller
+      <Button variant="outlined" >Outlined</Button>
     </div>
   );
 }
